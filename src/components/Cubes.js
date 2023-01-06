151906b49a42214d124461df21293e990d4de010
Szűcs Tibor <szutibo@gmail.com>
2023-01-06 16:07:34 +0100

    Project finished

---
+++ src/components/Cubes.js
@@ -0,0 +1,18 @@
+import { useStore } from '../hooks/useStore';
+import { Cube } from '../components/Cube';
+
+export const Cubes = () => {
+    const [cubes] = useStore((state) => [
+        state.cubes
+    ]);
+
+    return cubes.map(({ key, pos, texture }) => {
+        return (
+            <Cube
+                key={key}
+                position={pos}
+                texture={texture}
+            />
+        )
+    })
+}
